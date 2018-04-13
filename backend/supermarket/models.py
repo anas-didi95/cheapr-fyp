7e329df9e5de3f961b7d307094934b13d1264622
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-13 11:02:31 +0800

    update backend supermarket models

--- backend/supermarket/models.py
+++ backend/supermarket/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Supermarket(models.Model):
+  """Model for Supermarket"""
+  name = models.CharField(max_length=255, blank=False, unique=True)
+  date_created = models.DateTimeField(auto_now_add=True)
+  date_updated = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return '%s' % (self.name);
