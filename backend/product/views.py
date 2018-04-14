4822929b2877dc99b96ecf7b19cf4da56b6973ba
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-14 22:58:06 +0800

    update backend product views

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+class ListCreateProductView(generics.ListCreateAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+
+class RetrieveUpdateDestroyProductView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
