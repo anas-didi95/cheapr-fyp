772d90b2d1e484ac9d0bd8bcbe6796180453db46
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-23 11:23:39 +0800

    update frontend container leftpanel

---
+++ frontend/src/containers/LeftPanel.vue
@@ -0,0 +1,25 @@
+<template>
+<aside id="left-panel" class="left-panel">
+  <nav class="navbar navbar-expand-sm navbar-default">
+    <NavbarHeader/>
+    <MainMenu/>
+  </nav>
+</aside>
+</template>
+
+<script>
+import NavbarHeader from '@/components/NavbarHeader';
+import MainMenu from '@/components/MainMenu';
+export default {
+  name: "LeftPanel",
+  components: {
+    NavbarHeader,
+    MainMenu,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
+
