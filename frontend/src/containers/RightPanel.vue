bfe0e72a0a1e8f558048c2101399171b906d3b6e
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-23 11:23:50 +0800

    update frontend container rightpanel

---
+++ frontend/src/containers/RightPanel.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="right-panel" class="right-panel">
+  <Header></Header>
+  <Breadcrumb></Breadcrumb>
+  <div class="content mt-3"></div>
+</div>
+
+</template>
+
+<script>
+import Header from '@/components/Header';
+import Breadcrumb from '@/components/Breadcrumb';
+export default {
+  name: 'RightPanel',
+  components: {
+    Header,
+    Breadcrumb,
+  }
+}
+</script>
+
+<style>
+
+</style>
