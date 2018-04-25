993db86b428e50e2ab43bcfedd1c39b26cf232a3
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-26 06:04:43 +0800

    update frontend view product details

---
+++ frontend/src/views/ProductDetails.vue
@@ -0,0 +1,35 @@
+<template>
+ <div>
+   <div class="row">
+     <div class="col-sm-4 offset-sm-4">
+       <ItemCard :item='item'></ItemCard>
+     </div>
+   </div>
+ </div> 
+</template>
+
+<script>
+import ItemCard from '@/components/ItemCard'
+export default {
+  name: 'ProductDetails',
+  components: {
+    ItemCard
+  },
+  data: function () {
+    return {
+      item: {
+        "id": 1,
+        "name": "100 Plus Isotonic Drink",
+        "category": "Drink",
+        "category_name": "Drink",
+        "date_created": "2018-04-25T21:05:22.482223Z",
+        "date_updated": "2018-04-25T21:05:22.482259Z"
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
