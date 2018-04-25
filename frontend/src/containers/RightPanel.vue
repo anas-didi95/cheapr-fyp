1544dc5c05448d3602dd71435e6d535b9535b732
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-26 05:10:28 +0800

    update frontend container right panel

--- frontend/src/containers/RightPanel.vue
+++ frontend/src/containers/RightPanel.vue
@@ -1,8 +1,10 @@
 <template>
 <div id="right-panel" class="right-panel">
   <Header></Header>
-  <Breadcrumb></Breadcrumb>
-  <div class="content mt-3"></div>
+  <Breadcrumb :text='breadcrumb'></Breadcrumb>
+  <div class="content mt-3">
+    <ProductList></ProductList>
+  </div>
 </div>
 
 </template>
@@ -10,11 +12,18 @@
 <script>
 import Header from '@/components/Header'
 import Breadcrumb from '@/components/Breadcrumb'
+import ProductList  from '@/views/ProductList'
 export default {
   name: 'RightPanel',
   components: {
     Header,
-    Breadcrumb
+    Breadcrumb,
+    ProductList
+  },
+  data: function () {
+    return {
+      breadcrumb: 'Product List'
+    }
   }
 }
 </script>
