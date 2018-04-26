bcefdeec46b84802f8a097f07c6ba63a9629b105
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-27 04:31:38 +0800

    update backend product urls

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
   path('', views.ListCreateProductView.as_view(), name='product.l-c'),
   path('<int:pk>', views.RetrieveUpdateDestroyProductView.as_view(), name='product.r-u-d'),
+  path('search', views.ListSearchView.as_view(), name='product-search.l'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
