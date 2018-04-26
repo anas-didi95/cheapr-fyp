78bce371457390cc258e235b38a027882f36c578
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-26 17:40:10 +0800

    update router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
+
+const ProductList = () => import('@/views/ProductList')
+const ProductDetails = () => import('@/views/ProductDetails')
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/product',
+      name: 'product-list',
+      component: ProductList,
+      meta: {
+        breadcrumb: {
+          title: 'Application',
+          id: 'product-list',
+          name: 'Product List'
+        }
+      }
+    },
+    {
+      path: '/product/:id',
+      name: 'product-details',
+      component: ProductDetails,
+      meta: {
+        breadcrumb: {
+          title: 'Application',
+          id: 'product-details',
+          name: 'Product Details',
+          hasPrevious: true,
+          previous: {name: 'product-list'}
+        }
+      }
     }
   ]
 })
