1c957018517e401b15f23000bb2f81b48b057c43
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-27 06:09:52 +0800

    update frontend view productlist

--- frontend/src/views/ProductList.vue
+++ frontend/src/views/ProductList.vue
@@ -19,7 +19,10 @@
     </div>
   </div>
   <div class="row">
-    <div class="col-sm-4 col-6" v-for="item in searchItem" :key='item.id'>
+    <div class="col-12 text-center" v-if="this.loadingAjax">
+      <img :src="loadingImg" alt="Loading...">
+    </div>
+    <div class="col-sm-4 col-6" v-else v-for="item in searchItem" :key='item.id'>
       <router-link :to="{name: 'product-details', params: {id: item.id} }">
         <ItemCard :item='item'></ItemCard>
       </router-link>
@@ -29,6 +32,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+import loadingImg from '@/assets/ajax-loader.gif'
 import ItemCard from '@/components/ItemCard'
 export default {
   name: 'ProductList',
@@ -38,7 +43,21 @@ export default {
   data: function () {
     return {
       title: '',
-      items
+      items: [],
+      search: '',
+      loadingAjax: false,
+      loadingImg: loadingImg
+    }
+  },
+  methods: {
+    getProducts: function () {
+      this.loadingAjax = true
+      axios.get('http://localhost:8000/product/')
+        .then((response) => {
+          console.log(response.statusText)
+          this.items = response.data
+          this.loadingAjax = false
+        })
     }
   },
   computed: {
@@ -48,6 +67,9 @@ export default {
         return item.name.toLowerCase().indexOf(search) !== -1
       })
     }
+  },
+  created () {
+    this.getProducts()
   },
   mounted () {
     this.title = this.$route.meta.breadcrumb.name
