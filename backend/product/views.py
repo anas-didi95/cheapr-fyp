65db8f9309f2c2b5141ae8997b5216ec0272e5a6
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-27 04:31:46 +0800

    update backend product views

--- backend/product/views.py
+++ backend/product/views.py
@@ -9,3 +9,12 @@ class ListCreateProductView(generics.ListCreateAPIView):
 class RetrieveUpdateDestroyProductView(generics.RetrieveUpdateDestroyAPIView):
   queryset = Product.objects.all()
   serializer_class = ProductSerializer
+
+class ListSearchView(generics.ListAPIView):
+  serializer_class = ProductSerializer
+
+  def get_queryset(self):
+    queryset = Product.objects.all()
+    query = self.request.query_params.get('category')
+    queryset = queryset.filter(category__iexact=query)
+    return queryset
