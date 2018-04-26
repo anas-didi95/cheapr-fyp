b2b799be410b270628fc08557f4ab10933e0cc75
Anas Juwaidi <anas.didi95@gmail.com>
2018-04-26 17:40:05 +0800

    update frontend view product list

--- frontend/src/views/ProductList.vue
+++ frontend/src/views/ProductList.vue
@@ -3,6 +3,9 @@
   <div class="row">
     <div class="col-sm-6 offset-sm-3 col-12">
       <div class="card">
+        <div class="card-header">
+          <strong class="card-title mb-3 text-center">{{ title }}</strong>
+        </div>
         <div class="card-body">
           <div class="input-group">
             <div class="input-group-addon">
@@ -17,21 +20,24 @@
   </div>
   <div class="row">
     <div class="col-sm-4 col-6" v-for="item in searchItem" :key='item.id'>
-      <div class="card">
-        <div class="card-body">
-          {{ item.name }}
-        </div>
-      </div>
+      <router-link :to="{name: 'product-details', params: {id: item.id} }">
+        <ItemCard :item='item'></ItemCard>
+      </router-link>
     </div>
   </div>
 </div>
 </template>
 
 <script>
+import ItemCard from '@/components/ItemCard'
 export default {
   name: 'ProductList',
+  components: {
+    ItemCard
+  },
   data: function () {
     return {
+      title: '',
       items: [
         {
           "id": 1,
@@ -69,6 +75,9 @@ export default {
         return item.name.toLowerCase().indexOf(search) !== -1
       })
     }
+  },
+  mounted () {
+    this.title = this.$route.meta.breadcrumb.name
   }
 }
 </script>
