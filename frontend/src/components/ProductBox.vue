475bb2e5aff5345986f4dcd164ed1a61da86b35e
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-05 11:21:44 +0800

    create frontend component productbox

---
+++ frontend/src/components/ProductBox.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="productbox">
+  <img src="http://placehold.it/460x250/e67e22/ffffff&text=HTML5" class="img-fluid">
+  <div class="producttitle">{{ item.name }}</div>
+  <div class="productprice">
+    <div class="pull-right">
+      <router-link :to='{name: "product-details", params: {id: item.id} }' class="btn btn-danger">
+        <span>View</span>
+      </router-link>
+    </div>
+    <div class="pricetext">{{ item.category }}</div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ProductBox',
+  props: ['item']
+}
+</script>
+
+<style>
+.productbox {
+  background-color:#ffffff;
+	padding: 20px;
+	margin-bottom:10px;
+  /* margin: 5px; */
+	-webkit-box-shadow: 0 8px 6px -6px  #999;
+	   -moz-box-shadow: 0 8px 6px -6px  #999;
+	        box-shadow: 0 8px 6px -6px #999;
+}
+.producttitle {
+    font-weight:bold;
+	padding:5px 0 5px 0;
+}
+.productprice {
+	border-top:1px solid #dadada;
+	padding-top:5px;
+}
+.pricetext {
+	font-weight:bold;
+	font-size:0.8em;
+}
+</style>
