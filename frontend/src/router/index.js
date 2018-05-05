2e31b85dc22363f1602c1ea32a337065797506b7
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-05 21:06:09 +0800

    update frontend router index

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,36 +3,48 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const ProductList = () => import('@/views/ProductList')
-const ProductDetails = () => import('@/views/ProductDetails')
+// const ProductList = () => import('@/views/ProductList')
+// const ProductDetails = () => import('@/views/ProductDetails')
+const ProductSearch = () => import('@/containers/ProductSearch')
+const ProductDetails = () => import('@/containers/ProductDetails')
 
 export default new Router({
   routes: [
     {
-      path: '/product',
-      name: 'product-list',
-      component: ProductList,
-      meta: {
-        breadcrumb: {
-          title: 'Application',
-          id: 'product-list',
-          name: 'Product List'
-        }
-      }
+      path: '/',
+      name: 'product-search',
+      component: ProductSearch
     },
     {
       path: '/product/:id',
       name: 'product-details',
-      component: ProductDetails,
-      meta: {
-        breadcrumb: {
-          title: 'Application',
-          id: 'product-details',
-          name: 'Product Details',
-          hasPrevious: true,
-          previous: {name: 'product-list'}
-        }
-      }
+      component: ProductDetails
     }
+    // {
+      // path: '/product',
+      // name: 'product-list',
+      // component: ProductList,
+      // meta: {
+        // breadcrumb: {
+          // title: 'Application',
+          // id: 'product-list',
+          // name: 'Product List'
+        // }
+      // }
+    // },
+    // {
+      // path: '/product/:id',
+      // name: 'product-details',
+      // component: ProductDetails,
+      // meta: {
+        // breadcrumb: {
+          // title: 'Application',
+          // id: 'product-details',
+          // name: 'Product Details',
+          // hasPrevious: true,
+          // previous: {name: 'product-list'}
+        // }
+      // }
+    // }
   ]
 })
