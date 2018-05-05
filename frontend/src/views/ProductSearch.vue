ba0840c64970218d48981a46100574110b9289e1
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-05 21:06:26 +0800

    update frontend view productsearch

---
+++ frontend/src/views/ProductSearch.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <search :value='value'/>
+  <hr>
+  <product-list/>
+</div>
+</template>
+
+<script>
+import Search from '@/containers/Search'
+import ProductList from '@/containers/ProductList'
+export default {
+  name: 'ProductSearch',
+  components: {
+    Search,
+    ProductList
+  },
+  data: function () {
+    return {
+      value: 'hello world'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
