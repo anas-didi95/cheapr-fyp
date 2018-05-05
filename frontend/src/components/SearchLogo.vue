c32d2a1b4ad727a4528491a7701940584f3e846c
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-05 21:04:03 +0800

    update frontend component searchlogo

---
+++ frontend/src/components/SearchLogo.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <div id="logo" class="text-center">
+    <h1>Cheapr</h1>
+    <p>.:: Product Analyzer ::.</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'SearchLogo'
+}
+</script>
+
+<style>
+#logo {
+color: #666;
+width:100%;
+}
+#logo h1 {
+    font-size: 60px;
+    text-shadow: 1px 2px 3px #999;
+    font-family: Roboto, sans-serif;
+    font-weight: 700;
+    letter-spacing: -1px;
+}
+#logo p {
+  padding-bottom: 20px;
+}
+</style>
