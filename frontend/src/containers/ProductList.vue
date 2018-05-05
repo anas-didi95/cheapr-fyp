27cc88456009fab494b10bf6b93a9d9067e96f6e
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-05 10:46:22 +0800

    create frontend container productlist

---
+++ frontend/src/containers/ProductList.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="container">
+  <div class="row">
+		<ProductBox/>
+  </div>
+</div>
+</template>
+
+<script>
+import ProductBox from '@/components/ProductBox'
+export default {
+	name: 'ProductList',
+	components: {
+		ProductBox
+	}
+};
+</script>
+
+<style>
+
+</style>
