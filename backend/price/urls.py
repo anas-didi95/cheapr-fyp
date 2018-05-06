1e8d17ccd5816cc2fb26ffec9a7451617cba17e7
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-06 10:55:48 +0800

    update backend price urls

--- backend/price/urls.py
+++ backend/price/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
   path('', views.ListCreatePriceView.as_view(), name='price.l-c'),
   path('<int:pk>', views.RetrieveUpdateDestroyPriceView.as_view(), name='price.r-u-d'),
+  path('filter', views.ListFilterView.as_view(), name='price-filter.l'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
