a9123bf6528f7a8d0efb105ec28f0259b3137d5e
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-06 10:55:29 +0800

    update backend price views

--- backend/price/views.py
+++ backend/price/views.py
@@ -9,3 +9,15 @@ class ListCreatePriceView(generics.ListCreateAPIView):
 class RetrieveUpdateDestroyPriceView(generics.RetrieveUpdateDestroyAPIView):
   queryset = Price.objects.all()
   serializer_class = PriceSerializer
+
+class ListFilterView(generics.ListAPIView):
+  serializer_class = PriceSerializer
+
+  def get_queryset(self):
+    queryset = Price.objects.all()
+    query_product = self.request.query_params.get('product')
+    query_supermarket = self.request.query_params.get('supermarket')
+    queryset = queryset.filter(
+      product_id=query_product,
+      supermarket_id=query_supermarket)
+    return queryset
