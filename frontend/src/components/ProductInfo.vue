2ccba53e2723498e905df87ed72a41f5cdb3dd78
Anas Juwaidi <anas.didi95@gmail.com>
2018-05-06 17:04:26 +0800

    update frontend component productinfo

--- frontend/src/components/ProductInfo.vue
+++ frontend/src/components/ProductInfo.vue
@@ -1,42 +1,57 @@
 <template>
 <article class="card-body p-5">
-  <h3 class="title mb-3">{{ item.name }}</h3>
+  <h3 class="title mb-3">{{ product.name }}</h3>
   <p class="price-detail-wrap">
-    <div class="price h3 text-warning" v-for='price in this.prices' :key='price.id' v-if='item'>
-      <span class="currency">{{ price.supermarket }} | RM </span>
-      <span class="num">{{ price.price_value }}</span>
-      <span>/per {{ price.description }}</span>
-    </div>
+    <table class="table">
+      <tr v-for='price in this.display_prices' :key='price.id'>
+        <td>{{ price.name }}</td>
+        <td>RM {{ price.value.price_value }}</td>
+      </tr>
+    </table>
   </p>
   <dl class="item-property">
     <dt>Description</dt>
     <dd>
-      <p>{{ item.name }} {{ item.prices[0].description }}</p>
+      <p>{{ product.name }} {{ this.description }}</p>
     </dd>
   </dl>
   <dl class="param param-feature">
     <dt>Latest update</dt>
-    <dd>{{ this.date_updated }}</dd>
+    <dd>{{ this.latest_update }}</dd>
   </dl>
 </article>
 </template>
 
 <script>
 export default {
-  name: 'ProductInfo',
-  props: ['item'],
-  computed: {
-    date_updated: function () {
-      let datetime = this.item.prices[0].date_updated
-      return new Date(datetime).toDateString()
-    },
-    prices: function () {
-      let prices = []
-      for (let i = 0; i < 3; i++) {
-        prices.push(this.item.prices[i])
+  name: "ProductInfo",
+  props: ["product", "supermarkets", "prices", "loadingAjax"],
+  data: function() {
+    return {
+      description: "",
+      latest_update: "",
+      display_prices: null
+    }
+  },
+  watch: {
+    loadingAjax: function(ajax) {
+      if (!ajax) {
+        this.getLatestDate();
+        this.getDisplayPrices();
       }
-      console.log(prices)
-      return prices
+    }
+  },
+  methods: {
+    getLatestDate: function() {
+      let v = this.prices[0]
+      this.description = v.value.description
+      this.latest_update = `${v.value.year_start}/${v.value.month_start}/${v.value.day_start}`
+    },
+    getDisplayPrices: function () {
+      this.display_prices = this.prices
+      this.display_prices.sort((a,b) => {
+        return parseFloat(a.value.price_value) - parseFloat(b.value.price_value)
+      })
     }
   }
 }
